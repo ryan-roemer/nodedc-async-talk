5e13c2fe722a7e9f13a7b413bc26fadf67122aed
Ryan Roemer <ryan@loose-bits.com>
2012-02-19 08:24:15 -0500

    Initial import.

---
+++ README.md
@@ -0,0 +1,9 @@
+Node.DC Talk - Async.js
+=======================
+Talk presentation and demo materials for my 2/20/2012 Node.DC lightning talk
+on [async.js](https://github.com/caolan/async).
+
+Licenses
+========
+All demo code is Copyright 2012 Ryan Roemer. Released under MIT License.
+Impress.js is Copyright 2011-2012 Bartek Szopka. Released under MIT License.
---
+++ code/copy.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var fs = require('fs'),
+  path = require('path'),
+  file1 = "file1.txt",
+  file2 = "file2.txt";
+
+// Check if file1 exists, write to file2,
+// then display new file2 contents.
+path.exists(file1, function (exists) {
+  if (!exists) throw new Error("No file!");
+
+  fs.readFile(file1, function (err, data) {
+    if (err) throw err;
+
+    fs.writeFile(file2, data, function (err) {
+      if (err) throw err;
+
+      fs.readFile(file1, function (err, data) {
+        if (err) throw err;
+        console.log(data.toString());
+      });
+    });
+  });
+});

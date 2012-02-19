eacf05f27d35d028dcc60cd157d015f2f949ad68
Ryan Roemer <ryan@loose-bits.com>
2012-02-19 11:48:23 -0500

    Nested example.

---
+++ code/nested.js
@@ -0,0 +1,21 @@
+fun1(params, function (err, results) {
+  fun2(params, function (err, results) {
+    fun3(params, function (err, results) {
+      fun4(params, function (err, results) {
+        fun5(params, function (err, results) {
+          fun6(params, function (err, results) {
+            fun7(params, function (err, results) {
+              fun8(params, function (err, results) {
+                fun9(params, function (err, results) {
+                  fun10(params, function (err, results) {
+                    // Do something interesting here.
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
